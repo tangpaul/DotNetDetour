099ddf4a5224851c655593b5d6bb1e9b701cd8c2
tangpeng <tp18819491316@outlook.com>
2020-05-23 12:20:45 +0800

    增加.net core3.1框架的实现

---
+++ DotNetCoreDetour/DetourFactory.cs
@@ -0,0 +1,30 @@
+﻿using DotNetCoreDetour.DetourWays;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCoreDetour
+{
+    public class DetourFactory
+    {
+        public static IDetour CreateDetourEngine()
+        {
+            if (IntPtr.Size == 4)
+            {
+                return new NativeDetourFor32Bit();
+            }
+            else if (IntPtr.Size == 8)
+            {
+                return new NativeDetourFor64Bit();
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
+
